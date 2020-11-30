957d3db8dad3f03da394791ed9622ab1a93e14d0
Julien Cayzac <julien.cayzac@gmail.com>
2020-11-30 17:35:20 +0900

    add some debug info in html comments

--- docs/vendor/js/index.js
+++ docs/vendor/js/index.js
@@ -66,8 +66,30 @@ function editPage(hook, vm) {
   })
 }
 
+function debugInfo(hook, vm) {
+  const replacer = () => {
+    const seen = new WeakSet()
+    return (_, value) => {
+      if (typeof value === "object" && value !== null) {
+        if (seen.has(value)) {
+          return
+        }
+        seen.add(value)
+      }
+      return value
+    }
+  }
+
+  hook.afterEach(function(html) {
+    return `${html}<!-- Debug information:
+${JSON.stringify(vm, replacer(), 2)}
+-->`
+  })
+}
+
 window.$docsify.plugins = (window.$docsify.plugins || []).concat([
   editPage,
+  debugInfo,
 ])
 
 navigator.serviceWorker?.register(`${basePath}vendor/js/service-worker.js`)
