ae1fef7ebd4aefa708a6dfebd54f1c68ec566d1d
Julien Cayzac <julien.cayzac@gmail.com>
2020-12-02 15:20:57 +0900

    embed default css in code-line-highlights plugin

--- docs/vendor/docsify-code-line-highlights/index.js
+++ docs/vendor/docsify-code-line-highlights/index.js
@@ -1,5 +1,29 @@
 'use strict'
 ;(function (window) {
+  const css = `
+  pre[data-line] {
+    position: relative;
+  }
+
+  pre[data-line] .line-highlight {
+    position: absolute;
+    margin: var(--code-block-padding);
+    margin-left: 0;
+    margin-right: 0;
+    left: 0;
+    right: 0;
+    height: var(--code-block-line-height);
+    pointer-events: none;
+    padding: 0;
+    background: linear-gradient(to right, hsla(var(--theme-hue), var(--theme-saturation), var(--theme-lightness),.15) 60%, hsla(var(--theme-hue), var(--theme-saturation), var(--theme-lightness),0) 90%);
+    border-left: 4px solid var(--theme-color);
+  }
+  `
+
+  const { document } = window
+  const style = document.head.insertBefore(document.createElement('style'), document.head.firstElementChild)
+  if (style.styleSheet) style.styleSheet.cssText = css
+  else style.appendChild(document.createTextNode(css))
 
   function HighlightedCodeLines(hook, vm) {
     hook.mounted(function() {
